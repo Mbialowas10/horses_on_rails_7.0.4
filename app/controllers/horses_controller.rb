6de228baf3921ecfe63616d8b3309b724662a5c1
mbialowas10 <mbialowas@rrc.ca>
2023-10-18 11:09:13 -0500

    added implementation to breed and horse controllers

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -1,5 +1,11 @@
 class HorsesController < ApplicationController
-  def index; end
+  # GET /horses/:id
+  def show
+    @horse = Horse.find(params[:id])
+  end
 
-  def show; end
+  # GET /horses/search?keywords=user+search+terms
+  def search
+    @horses = Horse.where('name LIKE ?', "%#{params[:search_term]}%")
+  end
 end
